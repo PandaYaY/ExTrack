d78b51d246473ccdc00ee170f8b4bc9e08526d00
Артем Галимьянов <artem-gali@mail.ru>
2025-10-02 16:59:02 +0300

    refactor db

--- db/Migrations/001_public/001_tables/001_roles/002_fill.sql
+++
@@ -1,8 +0,0 @@
-﻿--liquibase formatted sql
-
---changeset agalimianov:1
-insert into public.roles (id, name)
-values (1, 'Admin'),
-       (2, 'User')
-on conflict (id) do nothing;
---rollback delete from public.roles where id in (1, 2);
